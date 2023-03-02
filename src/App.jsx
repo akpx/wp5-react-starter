0c303722880976a818789ee6bc2c230a693e7dad
Abhishek Pathak <akpathak.us@gmail.com>
2023-03-02 16:35:04 -0500

    Add React

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world!!</h1>);
